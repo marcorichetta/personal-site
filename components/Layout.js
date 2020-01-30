5529b813191c54474b329e90c070949aadc26e71
Marco Richetta <marcorichetta@gmail.com>
2020-01-30 20:40:56 -0300

    Added favicon, compressed fonts and potato image

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,12 @@ export default ({ children, title = 'Marco Richetta - Programador Web' }) => {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        {/* Favicons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="icons/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="icons/favicon/site.webmanifest" />
       </Head>
       <div id='NextApp' className="flex flex-col justify-between min-h-screen text-gray-300">
         <Navbar />
