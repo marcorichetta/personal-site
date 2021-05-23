1ac74a89e3ec695fe51b338671c3e3a3f324c1ab
Marco Richetta <marcorichetta@gmail.com>
2021-05-23 02:23:36 -0300

    Self host Inter variable font
    - <CustomHead /> tiene los tags que cambian por sitio
    - _document tiene los tags para todo el sitio
    - Config para cache de la font por un año (vercel.json)

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # My personal site
 
-Sitio personal hecho con [Tailwind CSS](https://tailwindcss.com) y [Next.js](https://nextjs.org/).
+Sitio personal hecho con [Next.js](https://nextjs.org/) y [Tailwind CSS](https://tailwindcss.com).
+
+## Resumen
+
+-   `pages/` - Home
+
+    -   `index.tsx` - Home
+
+    -   `_app.tsx` - Inyecta funcionalidad compartida por todas las páginas del sitio. En mi caso la uso para cargar los `index.css`. [Custom App - Nextjs](https://nextjs.org/docs/advanced-features/custom-app)
+
+    -   `_document.tsx` - Permite modificar HTML generado por Next en el server. Yo lo uso específicamente para el `<Head />` del sitio. [Custom Document - Nextjs](https://nextjs.org/docs/advanced-features/custom-document)
+
+    -   `pages/blog/*` - Páginas del blog que cargan el contenido de `data/blog/` con [MDX](https://github.com/mdx-js/mdx).
+
+    -   `*` - Demás páginas estáticas
 
 ## Hacé el tuyo
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,46 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
-
 	render() {
 		return (
 			<Html lang="es">
-				<Head />
+				<Head>
+					{/* Self hosted Inter font */}
+					<link
+						rel="preload"
+						href="/fonts/inter-var-latin.woff2"
+						as="font"
+						type="font/woff2"
+						crossOrigin="anonymous"
+					/>
+					{/* Favicons */}
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="icons/favicon/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="icons/favicon/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="icons/favicon/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="icons/favicon/site.webmanifest" />
+
+					{/* Google */}
+					<meta
+						name="google-site-verification"
+						content="0tR5wiUIN_xrDarPhRVBb-5Z7aI6nFMpWjxy_8zuR-E"
+					/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
