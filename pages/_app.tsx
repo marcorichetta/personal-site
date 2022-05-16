05d54522a2006b975ee0d08eb54baf272f9fa301
Marco Richetta <marcorichetta@gmail.com>
2022-05-16 18:12:29 -0300

    Web vitals integration with axiom https://vercel.com/integrations/axiom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
+import { NextWebVitalsMetric } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
+}
+  
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
+  
+  if (!url) {
+    return;
+  }
+  
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  });
+  
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true });
+  }
 }
 
 export default MyApp;
