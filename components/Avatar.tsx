d0cfc427ab589d651a7f9d510085d025b2d18d77
Marco Richetta <marcorichetta@gmail.com>
2023-05-14 16:56:25 -0300

    remove cloudflare as image loader

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import cvImg from "../public/images/cv.jpg";
 
 const Avatar = () => {
 	return (
 		<div id="avatar">
 			<Image
 				alt="Foto de perfil"
-				src="cv.jpg"
+				src={cvImg}
 				className="h-32 w-32 md:h-40 md:w-40 rounded-full"
 				width={250}
 				height={250}
--- next.config.js
+++ next.config.js
@@ -1,20 +1,15 @@
 module.exports = {
-	images: {
-		loader: "cloudinary",
-		domains: ["res.cloudinary.com"],
-		path: "https://res.cloudinary.com/dacfp3qlk/",
-	},
 	i18n: {
 		locales: ["es", "en"],
 		defaultLocale: "es",
 	},
 	async redirects() {
 		return [
-		  {
-			source: '/blog',
-			destination: 'blog.marcorichetta.vercel.app/',
-			permanent: true,
-		  },
+			{
+				source: "/blog",
+				destination: "blog.marcorichetta.vercel.app/",
+				permanent: true,
+			},
 		];
-	  },
+	},
 };
