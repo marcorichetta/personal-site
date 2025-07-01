5651b4724c5d3575f1edb069c11ea912e28dc09a
Marco Richetta <marcorichetta@gmail.com>
2025-07-01 12:48:31 +0200

    blog redirect

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
 		return [
 			{
 				source: "/blog",
-				destination: "https://blog.marcorichetta.vercel.app/",
+				destination: "https://blog.marcorichetta.dev/",
 				permanent: true,
 			},
 		];
